e21d3d75a75d5633979622d7f8d17bd315fd70a4
gabriele <gab>
2016-10-15 23:02:35 +0200

    a more realistic and optimized version of workshop_01

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -12,7 +12,7 @@ def createSpace(beamDimensions,
         beamDimensions: a tuple containing beam section dimension (x, z)
         pillarDimensions: a tuple containing pillar section dimension (x, y)
         axesDistances: a list containing distances between pillar axes
-        interstoryHeights: a list containing interstory heights 
+        interstoryHeights: a list containing interstory heights
     Returns:
         a HPC value describing the space frame parameterized by the arguments
 
@@ -21,23 +21,37 @@ def createSpace(beamDimensions,
     bx, bz = beamDimensions
     px, py = pillarDimensions
 
-    sections = []
+    floors = []
 
-    for distance in axesDistances:
-        beamGroup = []
-        structureHeight = bz  #to account for last beam
+    beams = None   # struct of beams
+    pillars = None # struct of pillars
 
-        for height in interstoryHeights:
-            structureHeight += height
-            beam = CUBOID([bx, distance, bz])
-            beamGroup += [T(3)(height), beam]
+    for height in interstoryHeights:
 
-        beams = STRUCT(beamGroup)
-        pillar = CUBOID([px, py, structureHeight])
-        sections += [pillar, T(2)(py), beams, T(2)(distance)]
+        # if structures don't exists we create them
+        if not beams and not pillars:
+            pillar = CUBOID([px, py, height])
+            beamsList = []
+            pillarsList = [pillar]
+            beamSize = py / 2.0 # the first beam is half py size wider than the
+                                # others so we account for this
 
-    # last pillar
-    pillar = CUBOID([px, py, structureHeight])
-    sections.append(pillar)
+            for i in range(0, len(axesDistances) - 1):
+                distance = axesDistances[i]
+                beamSize += distance + py
+                beamsList += [CUBOID([bx, beamSize, bz]), T(2)(beamSize)]
+                pillarsList += [T(2)(distance + py), pillar]
+                beamSize = 0
 
-    return STRUCT(sections)
+            # the last beam has the same width as the first
+            distance = axesDistances[-1]
+            beamsList.append(CUBOID([bx, distance + py + (py / 2.0), bz]))
+            pillarsList += [T(2)(distance + py), pillar]
+
+            # structures
+            beams = STRUCT(beamsList)
+            pillars = STRUCT(pillarsList)
+
+        floors += [pillars, T(3)(height), beams, T(3)(bz)]
+
+    return STRUCT(floors)
